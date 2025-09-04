208362548e3adaf2fdbfa31d572d1953c732acba
Nagapuri Swathi <83587716+NagapuriSwathi@users.noreply.github.com>
2025-09-04 12:31:18 +0530

    Added script.js via upload

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,18 @@
+const navigationUl = document.getElementsByTagName("ul");
+const hamburger = document.getElementsByClassName("hamburger");
+const close = document.getElementsByClassName("close");
+const currentYear = document.getElementById("current__year");
 
+hamburger[0].addEventListener("click", () => {
+    navigationUl[0].classList.add("navigation-md");
+    hamburger[0].style.display = "none";
+    close[0].style.display = "block";
+});
+
+close[0].addEventListener("click", () => {
+    navigationUl[0].classList.remove("navigation-md");
+    hamburger[0].style.display = "flex";
+    close[0].style.display = "none";
+});
+
+currentYear.innerText = new Date().getFullYear()
